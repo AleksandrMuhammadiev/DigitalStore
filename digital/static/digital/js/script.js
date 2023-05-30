35e7c435687fee0199805c62df03514d7610e315
Alexandr <muhammadievaleksandr29@gmail.com>
2023-05-30 16:20:54 +0500

    'digital_messages_none'

--- digital/static/digital/js/script.js
+++ digital/static/digital/js/script.js
@@ -1,7 +1,13 @@
+setTimeout(function(){
+document.getElementById('message').style.display = 'none';
+}, 3000);
+
+
+ 
 const orderBtn = document.querySelector('.my_orders')
-    const listOrders = document.querySelector('.list_orders')
-    const arrowDown = document.querySelector('.errow_down')
-    orderBtn.addEventListener('click', () => {
-      listOrders.classList.toggle('active')
-      arrowDown.classList.toggle('active')
-    })
+const listOrders = document.querySelector('.list_orders')
+const arrowDown = document.querySelector('.errow_down')
+orderBtn.addEventListener('click', () => {
+  listOrders.classList.toggle('active')
+  arrowDown.classList.toggle('active')
+});
